e581a0c2c4a72ab381a45be1f25c91b734786a8e
2BTuby <tubychrist@gmail.com>
2026-02-03 12:44:57 +0000

    Skeleton CrimeMap

---
+++ dbhelper.py
@@ -0,0 +1,38 @@
+from getpass import getpass
+import pymysql
+
+
+class DBHelper:
+    def connect(self, database="crimemap"):
+        password = getpass()
+        return pymysql.connect(host='localhost', user='remote_user', passwd=password, db=database)
+
+    def get_all_inputs(self):
+        connecttion = self.connect()
+        try:
+            query = "SELECT description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            return cursor.fetchall()
+        finally:
+            connection.close()
+
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
+
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+        finally:
+            connection.close()
