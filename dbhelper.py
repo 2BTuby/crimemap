936ea3de9b408faf1645b2e0ae1f2764d40b18a6
2BTuby <tubychrist@gmail.com>
2026-02-04 17:03:20 +0000

    database update

--- dbhelper.py
+++ dbhelper.py
@@ -1,14 +1,13 @@
-from getpass import getpass
+import dbconfig
 import pymysql
 
 
 class DBHelper:
     def connect(self, database="crimemap"):
-        password = getpass()
-        return pymysql.connect(host='localhost', user='remote_user', passwd=password, db=database)
+        return pymysql.connect(host='localhost', user='remote_user', passwd=dbconfig.db_password, db=database)
 
     def get_all_inputs(self):
-        connecttion = self.connect()
+        connection = self.connect()
         try:
             query = "SELECT description FROM crimes;"
             with connection.cursor() as cursor:
@@ -34,5 +33,17 @@ class DBHelper:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 connection.commit()
+        finally:
+            connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES(%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+                connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             connection.close()
